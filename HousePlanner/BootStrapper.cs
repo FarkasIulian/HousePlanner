1010966c296720b461eaa5ca455a132223e4677a
FarkasIulian <iulian.farkas@yahoo.com>
2024-05-23 01:50:12 +0300

    Implemented basic prism setup

---
+++ HousePlanner/BootStrapper.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.Xpf.Core;
+using HousePlannerCore;
+using Prism.DryIoc;
+using Prism.Events;
+using Prism.Ioc;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HousePlanner
+{
+    public class BootStrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            ApplicationThemeHelper.ApplicationThemeName = Theme.VS2019DarkName;
+            
+            var rm = Container.Resolve<IRegionManager>();
+            
+            rm.RegisterViewWithRegion<MainWindow>("MainWindowRegion");
+            
+            var shell = Container.Resolve<Shell>();
+            
+            return shell;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            EventAggregatorProvider.EventAggregator = Container.Resolve<IEventAggregator>();
+        }
+    }
+}
